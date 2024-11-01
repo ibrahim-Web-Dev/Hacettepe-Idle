42106d8bb9f02433f4bb1532e78bf18eb517a75d
ibrahim-Web-Dev <ibrahimyilmaz0713@gmail.com>
2024-11-01 10:15:38 +0300

    Make basic pages and some pictures for the foundations of the game

---
+++ script.js
@@ -0,0 +1,4 @@
+game_start_btn = document.querySelector(".game_starter");
+game_start_btn.addEventListener("click", function() {
+    window.location.href = "index1.html"; // Redirect to index1.html
+});
